9db768a22e6e77034fe7a808a205f6efb76bc05a
AlanHerreraMartinez <alanherreramartinez441@gmail.com>
2023-07-05 13:12:07 -0600

    CLASE 28/05/23 AHM

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,11 +2,14 @@ import {RouterModule, Routes} from '@angular/router'
 import {NgModule} from '@angular/core'
 import { IricComponent } from './grupos/iric/iric.component';
 import { OperasBasComponent } from './grupos/formularios/operas-bas/operas-bas.component';
+import { HomeComponent } from './home/home.component';
+import { AlumnoReactiveComponent } from './formularios/alumno-reactive/alumno-reactive.component';
 
 const routes:Routes=[
-    {path: '', redirectTo: '/home', pathMatch:'full'}, //Esta primera es la redireccion a home
+    {path: 'HomeComponent', component: HomeComponent}, //Esta primera es la redireccion a home pathMatch:'full'
     {path: 'IricComponent', component: IricComponent},
-    {path: 'OperasBasComponent', component: OperasBasComponent}
+    {path: 'OperasBasComponent', component: OperasBasComponent},
+    {path: 'AlumnoReactiveComponent', component: AlumnoReactiveComponent}
 ];
 
 @NgModule({
